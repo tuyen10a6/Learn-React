ef780609df8bd7ab7b952d5aeddd972d801ade64
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-07-28 23:30:07 +0700

    random gift using useState React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+
 
 function App() {
+    const gifts = [
+        'Iphone 15 ProMax',
+        'Iphone 14 ProMax',
+        'Macbook Pro M1 2020 13 inch'
+    ];
+    const [gift, setGifts] = useState('')
+    const randomGift = () => {
+        const index = Math.floor(Math.random() * gifts.length);
+        setGifts(gifts[index]);
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 32}}>
+          <h1>{gift !== '' ? gift : 'Chưa có phần thưởng'}</h1>
+           <button onClick={randomGift}> Chọn quà</button>
     </div>
   );
 }
