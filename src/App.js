0ca79c8ddcd9fade90ff07fb50d729c92eca6aff
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-17 23:16:19 +0700

    useState with (callback, [])

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import {useEffect} from "react";
 function App() {
 
     const [value, setValue] = useState('');
-
+    const [post, setPost] = useState([]);
     useEffect(() => {
-       document.title = value;
-    })
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(res => res.json())
+            .then(posts => {
+                setPost(posts);
+            })
+    }, [])
     return (
         <div>
             <input value={value} onChange={(e) => setValue(e.target.value)} type='text'/>
+            <ul>
+                {post.map((item, index) => (<li key={item.id}>{item.title}</li>))}
+            </ul>
         </div>
     )
 }
