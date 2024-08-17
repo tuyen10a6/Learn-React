41f274fbef05573ad6243d60fd13b1571c3f9288
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-17 22:57:15 +0700

    useEffect with (callback)

--- src/App.js
+++ src/App.js
@@ -1,64 +1,19 @@
-import React from 'react';
-import {useState} from "react";
 import Content from './Content'
+import {useState} from "react";
+import {useEffect} from "react";
 
-function App(key) {
+function App() {
+
     const [value, setValue] = useState('');
-    const [show, setShow] = useState(false);
-    const [listData, setListData] = useState(() => {
-        const storageData = JSON.parse(localStorage.getItem('listData'));
-        console.log(storageData);
-        if (storageData && storageData.length > 0) {
-            return storageData;
-        } else {
-            return [];
-        }
-    });
-    const changeValue = (event) => {
-        setValue(event.target.value);
-    }
 
-    const addItem = () => {
-        if (value.trim() !== '') {
-            setListData((prevState) => {
-                localStorage.setItem('listData', JSON.stringify([...prevState, value]));
-                return [...prevState, value]
-            })
-
-            setValue('');
-        } else {
-            return alert('vui long nhap gia tri')
-        }
-    }
-
-    const removeItem = (index) => {
-        const newList = listData.filter((item, i) => i !== index)
-        localStorage.setItem('listData', JSON.stringify(newList));
-        setListData(newList)
-        return newList
-    }
-    return (<div style={{textAlign: 'center', marginTop: "30px"}} className={'root'}>
-        <input onChange={changeValue} value={value} type="text"/>
-        <br/>
-        <button onClick={addItem} style={{marginTop: "20px"}} className={'btn btn-submit'}>
-            ONCLICK
-        </button>
-        <div className={'list-data'}>
-            <ul>
-                {listData.map((item, index) => (<li style={{listStyle: "none"}} key={index}>
-                    {item}
-                    <span onClick={() => removeItem(index)} style={{marginLeft: '15px'}}>X</span>
-                </li>))}
-            </ul>
+    useEffect(() => {
+       document.title = value;
+    })
+    return (
+        <div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} type='text'/>
         </div>
-        <button onClick={() => {
-            setShow(!show);
-        }}> Toogle
-        </button>
-        {
-            show && <Content/>
-        }
-    </div>)
+    )
 }
 
 export default App;
