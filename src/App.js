a522404088eb3f468b6b4ac13c96608b3dde8f4f
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-17 22:12:45 +0700

    mounted and unmounted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {useState} from "react";
+import Content from './Content'
 
 function App(key) {
     const [value, setValue] = useState('');
+    const [show, setShow] = useState(false);
     const [listData, setListData] = useState(() => {
         const storageData = JSON.parse(localStorage.getItem('listData'));
         console.log(storageData);
@@ -35,25 +37,28 @@ function App(key) {
         setListData(newList)
         return newList
     }
-    return (
-        <div style={{textAlign: 'center', marginTop: "30px"}} className={'root'}>
-            <input onChange={changeValue} value={value} type="text"/>
-            <br/>
-            <button onClick={addItem} style={{marginTop: "20px"}} className={'btn btn-submit'}>
-                ONCLICK
-            </button>
-            <div className={'list-data'}>
-                <ul>
-                    {listData.map((item, index) => (
-                        <li style={{listStyle: "none"}} key={index}>
-                            {item}
-                            <span onClick={() => removeItem(index)} style={{marginLeft: '15px'}}>X</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+    return (<div style={{textAlign: 'center', marginTop: "30px"}} className={'root'}>
+        <input onChange={changeValue} value={value} type="text"/>
+        <br/>
+        <button onClick={addItem} style={{marginTop: "20px"}} className={'btn btn-submit'}>
+            ONCLICK
+        </button>
+        <div className={'list-data'}>
+            <ul>
+                {listData.map((item, index) => (<li style={{listStyle: "none"}} key={index}>
+                    {item}
+                    <span onClick={() => removeItem(index)} style={{marginLeft: '15px'}}>X</span>
+                </li>))}
+            </ul>
         </div>
-    )
+        <button onClick={() => {
+            setShow(!show);
+        }}> Toogle
+        </button>
+        {
+            show && <Content/>
+        }
+    </div>)
 }
 
 export default App;
