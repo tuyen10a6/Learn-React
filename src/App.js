9d2397e6956769248a50f4908e3e2cadd57d66fe
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-19 00:24:02 +0700

    add event listener

--- src/App.js
+++ src/App.js
@@ -6,26 +6,46 @@ import {useEffect} from "react";
 const tabs = ['posts', 'comments', 'albums']
 
 function App() {
-    const [title, setTilte] = useState('')
     const [posts, setPosts] = useState([])
     const [type, setType] = useState('posts');
+    const [show, setShow] = useState(false);
     useEffect(() => {
-        console.log(type)
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
             .then(data => setPosts(data))
     }, [type])
 
-    return (
-        <div style={{textAlign: 'center'}}>
-            {tabs.map(tab => (<button style={type === tab ? {color: "blue"} : {}} onClick={() => setType(tab)}
-                                      key={tab}>{tab} </button>))}
-            <ul>
-                {posts.map((item, index) => (
-                    <li key={index}> {item.title || item.name}</li>
-                ))}
-            </ul>
-        </div>)
+    useEffect(() => {
+        const handleScroll = () => {
+            console.log("ok")
+            window.scrollY > 400 ? setShow(true) : setShow(false)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+    const handleClickscrollY = () => {
+        window.scrollTo(0, 0)
+    }
+
+    console.log('scroll')
+
+    return (<div style={{textAlign: 'center'}}>
+        {tabs.map(tab => (<button style={type === tab ? {color: "blue"} : {}} onClick={() => setType(tab)}
+                                  key={tab}>{tab} </button>))}
+        <ul>
+            {posts.map((item, index) => (<li key={index}> {item.title || item.name}</li>))}
+        </ul>
+        {show && (
+            <button onClick={handleClickscrollY} style={{
+                position: 'fixed',
+                bottom: 20,
+                right: 20
+            }}>
+                Show item
+            </button>
+        )}
+    </div>)
 
 }
 
