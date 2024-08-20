8d828e43d8978a28c6c4e6e22c22af0671cc623b
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-20 22:53:13 +0700

    clear timeout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import {useEffect, useState} from "react";
 
-
 function App() {
     const [value, setValue] = useState(180)
     useEffect(() => {
-        setTimeout(() => {
-            setValue(value - 1)
-            console.log('value', value)
+        const timeId = setTimeout(() => {
+            setValue(prevState => {
+                console.log('value time: ', prevState - 1)
+                return prevState - 1;
+            })
         }, 1000)
+
+        return () => clearTimeout(timeId)
+
     }, [value])
     return (<div className={'app'}>
         {value}
