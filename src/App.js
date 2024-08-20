8806befa3f6fbd6b663043cd78dc069cfa1e5a4d
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-20 22:45:13 +0700

    set timeout with useEffect

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
-import Content from './Content'
-import {useState} from "react";
-import {useEffect} from "react";
+import React from 'react'
+import {useEffect, useState} from "react";
 
-
-const tabs = ['posts', 'comments', 'albums']
 
 function App() {
-    const [posts, setPosts] = useState([])
-    const [type, setType] = useState('posts');
-    const [show, setShow] = useState(false);
+    const [value, setValue] = useState(180)
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then(res => res.json())
-            .then(data => setPosts(data))
-    }, [type])
-
-    useEffect(() => {
-        const handleScroll = () => {
-            console.log("ok")
-            window.scrollY > 400 ? setShow(true) : setShow(false)
-        }
-
-        window.addEventListener('scroll', handleScroll)
-    }, [])
-
-    const handleClickscrollY = () => {
-        window.scrollTo(0, 0)
-    }
-
-    console.log('scroll')
-
-    return (<div style={{textAlign: 'center'}}>
-        {tabs.map(tab => (<button style={type === tab ? {color: "blue"} : {}} onClick={() => setType(tab)}
-                                  key={tab}>{tab} </button>))}
-        <ul>
-            {posts.map((item, index) => (<li key={index}> {item.title || item.name}</li>))}
-        </ul>
-        {show && (
-            <button onClick={handleClickscrollY} style={{
-                position: 'fixed',
-                bottom: 20,
-                right: 20
-            }}>
-                Show item
-            </button>
-        )}
+        setTimeout(() => {
+            setValue(value - 1)
+            console.log('value', value)
+        }, 1000)
+    }, [value])
+    return (<div className={'app'}>
+        {value}
     </div>)
-
 }
 
 export default App;
