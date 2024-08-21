215703af015dfd6cf4f83fb2937d5ac5810f1ae6
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-08-21 23:04:10 +0700

    map data lession

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import {useEffect, useState} from "react";
 
-function App() {
-
-
-    const [avatar, setAvatar] = useState('');
-    useEffect(() => {
-        return () => {
-            avatar && URL.revokeObjectURL(avatar.preview);
-        }
-    }, [avatar]);
-    const handleChangeAvatar = (e) => {
-        let file = e.target.files[0];
-        console.log(URL.createObjectURL(file));
-        file.preview = URL.createObjectURL(file);
-        setAvatar(file)
-
+const lession = [
+    {
+        'id': 1,
+        'name': 'PHP ( Laravel)'
+    },
+    {
+        'id': 2,
+        'name': 'Javascript (React)'
+    },
+    {
+        'id': 3,
+        'name': 'Node.js'
     }
+]
 
-    return (<div style={{textAlign: 'center'}} className={'app-name'}>
-            <input onChange={handleChangeAvatar} type="file"/>
-            <img width={'80px'} src={avatar.preview}/>
+function App() {
+    const [lessionId, setLessionId] = useState(1);
+    return (
+        <div className={'app-name'}>
+            <ul>
+                {lession.map(item => (
+                    <li onClick={() => setLessionId(item.id)}
+                        style={{color: lessionId === item.id ? 'red' : 'black'}} key={item.id}>
+                        {item.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
