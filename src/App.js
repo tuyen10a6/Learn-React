02a588546fa569e26de928e8ab7a4f135a4fab0c
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2024-09-07 16:37:04 +0700

    learn useMemo F8

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, {useCallback} from 'react';
-import {useState} from "react";
-import Content from "./Content";
+import React, {useMemo, useState} from 'react';
 
 const App = () => {
-    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('')
+    const [data, setData] = useState([]);
+    const handleSubmit = () => {
+        console.log('handle submit')
+        setData([...data, {
+            name: name,
+            price: parseInt(price)
+        }])
+    }
 
-    const handleIncrease = useCallback(() => {
-        setCount(prevCount => prevCount + 1);
-    }, [])
+    // tránh lặp lại 1 logic không cần thiết khi component re-render
+    const total = useMemo(() => {
+        const result = data.reduce((result, product) => {
+            console.log('total sum')
+            return result + product.price
+        }, 0)
+        return result;
+    }, [data])
+
     return (
-        <div style={{padding: "0px auto"}}>
-            <Content onIncrease={handleIncrease}/>
-            <p>count: {count}</p>
+        <div className={'app-name'}>
+            <input placeholder='Họ tên' onChange={(e) => setName(e.target.value)} type='text'/>
+            <input placeholder='Giá tiền' onChange={(e) => setPrice(e.target.value)} type='text'/>
+            <p>total: {total}</p>
+            <button onClick={handleSubmit}>
+                add data
+            </button>
+            <ul>
+                {data && data.length > 0 ? data.map((item, index) =>
+                    <li key={index}>
+                        {item.name}: {item.price}
+                    </li>
+                ) : 'null'}
+            </ul>
         </div>
     )
 }
